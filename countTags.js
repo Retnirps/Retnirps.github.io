bb7bf819d5435ed6b142c3ee3fb7b82453401f19
Retnirps <44922573+Retnirps@users.noreply.github.com>
2020-12-10 23:38:58 +0300

    Update countTags.js

--- countTags.js
+++ countTags.js
@@ -1,11 +1,13 @@
-let items = document.getElementsByTagName("*");
-let tags = [];
-for (let i = 0; i < items.length; i++) {
-    tags[i] = items[i].tagName;
+function countTags() {
+    let items = document.getElementsByTagName("*");
+    let tags = [];
+    for (let i = 0; i < items.length; i++) {
+        tags[i] = items[i].tagName;
+    }
+    let uniqueTags = [...new Set(tags)];
+    let tagsCount = "";
+    for (let i = 0; i < uniqueTags.length; i++) {
+        tagsCount += uniqueTags[i] + ": " + document.getElementsByTagName(uniqueTags[i]).length + "\n";
+    }
+    alert(tagsCount);
 }
-let uniqueTags = [...new Set(tags)];
-let tagsCount = "";
-for (let i = 0; i < uniqueTags.length; i++) {
-    tagsCount += uniqueTags[i] + ": " + document.getElementsByTagName(uniqueTags[i]).length + "\n";
-}
-alert(tagsCount)
